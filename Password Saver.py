7acd4fb7342ad18e1125cde4d3f9447cf3915898
RSH-454 <rayansalhulaibah@gmail.com>
2026-02-19 23:33:44 +0300

    Created py file
    wrote the code of the input options and the functions that are needed for those options.
    
    - Still need to work on the SaveNew() function
    
    -Need to write the code for the ViewSaved() function.

---
+++ Password Saver.py
@@ -0,0 +1,26 @@
+print('[PASSWWORD SAVER]')
+input('Press enter to continue...\n')
+Option = input('Choose operation: \n 1. Save new password \n 2. View saved passwords \n 3. Exit') 
+
+def SaveNew():
+    print('Enter name of the account: ')
+    Account = input()
+    print('Enter password: ')
+    Password = input()
+    with open(Account, 'w') as f:
+        f.write("Account: " + Account + " " + "Password: " + Password)  
+
+def ViewSaved():
+    print()
+
+def Exit():
+ SystemExit
+
+if Option == '1':
+    SaveNew()
+
+elif Option == '2':
+    ViewSaved()
+
+elif Option == '3':
+    exit    

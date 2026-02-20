81d050db3b0e8b44a39f655979da11e75a3270d2
RSH-454 <rayansalhulaibah@gmail.com>
2026-02-20 03:38:14 +0300

    Fixes
    Fixed the ViewSaved() & SaveNew() functions.
    
    Still need to work on ViewSaved().

--- Password Saver.py
+++ Password Saver.py
@@ -2,23 +2,24 @@ print('[PASSWWORD SAVER]')
 input('Press enter to continue...\n')
 Option = input('Choose operation: \n 1. Save new password \n 2. View saved passwords \n 3. Exit') 
 
+FileName = 'SavedPasswords.txt'
 def SaveNew():
     print('Enter name of the account: ')
     Account = input()
     print('Enter password: ')
     Password = input()
-    with open(Account, 'w') as f:
+    with open(FileName, 'w') as f:
         f.write("Account: " + Account + " " + "Password: " + Password)  
 
 def ViewSaved():
     print('Enter the name of the account you want to view: ')
     Account = input()
-    with open(Account, 'r') as f:
+    with open(FileName, 'r') as f:
         content = f.read()
-        print(content)
-
-def Exit():
- SystemExit
+        if Account in content:
+            print(content)
+        else:
+            print("Account not found.")
 
 if Option == '1':
     SaveNew()
@@ -27,4 +28,4 @@ elif Option == '2':
     ViewSaved()
 
 elif Option == '3':
-    exit    
+    exit()
